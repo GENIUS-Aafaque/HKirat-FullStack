30b75d439560ee7f10779909abcb602d8dd89c9e
Md Aafaque Tabish <aafaque.tabish@gmail.com>
2023-10-16 00:23:07 +0530

    solved week1-expenditure-analysis

--- week-1/Week-1-assignment-with-tests/01-js/easy/expenditure-analysis.js
+++ week-1/Week-1-assignment-with-tests/01-js/easy/expenditure-analysis.js
@@ -8,8 +8,31 @@
   - `npm run test-expenditure-analysis`
 */
 
+function checkExist(transaction, categories) {
+  for (let i = 0; i < categories.length; i++) {
+    obj = categories[i];
+    if (Object.values(obj).includes(transaction.category)) {
+      return i;
+    }
+  }
+  return undefined;
+}
+
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  let categories = [];
+  transactions.forEach(function (transaction) {
+    let i = checkExist(transaction, categories);
+    if (i != undefined) {
+      categories[i].totalSpent += transaction.price;
+      // categories.push(category);
+    } else {
+      categories.push({
+        category: transaction.category,
+        totalSpent: transaction.price,
+      });
+    }
+  });
+  return categories;
 }
 
 module.exports = calculateTotalSpentByCategory;
