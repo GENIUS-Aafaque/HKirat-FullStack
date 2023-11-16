fcaafefd698a9821547b76478148210d4afa8ee1
Md Aafaque Tabish <aafaque.tabish@gmail.com>
2023-11-16 13:44:38 +0530

    Solved week 1.2 - hard -3

--- week-1/02-async-js/hard/3-promise-all.js
+++ week-1/02-async-js/hard/3-promise-all.js
@@ -6,17 +6,37 @@
 
 
 function waitOneSecond() {
-
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+        }, 1000)
+    })
 }
 
 function waitTwoSecond() {
-
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+        }, 2000)
+    })
 }
 
 function waitThreeSecond() {
-
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+        }, 3000)
+    })
 }
 
 function calculateTime() {
-
+    let start = new Date().getTime();
+    return Promise.all([waitOneSecond(), waitTwoSecond(), waitThreeSecond()])
+        .then(() => {
+            let end = new Date().getTime();
+            return ((end - start) / 1000);
+        });
 }
+calculateTime().then((time) => {
+    console.log("Completed in : " + time + " seconds.")
+})
