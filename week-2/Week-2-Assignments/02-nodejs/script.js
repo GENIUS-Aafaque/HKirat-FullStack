b764509d05b51e2e1253aa23feafacb18e47f0d3
Md Aafaque Tabish <aafaque.tabish@gmail.com>
2023-11-21 00:58:10 +0530

    Add : postTodos function to add new todo

--- week-2/Week-2-Assignments/02-nodejs/script.js
+++ week-2/Week-2-Assignments/02-nodejs/script.js
@@ -28,3 +28,40 @@ function getTodos() {
 }
 getTodos();
 
+function postTodos() {
+    const title = document.getElementById("title").value;
+    const description = document.getElementById("description").value;
+    fetch("http://localhost:3000/todos", {
+        method: "POST",
+        body: JSON.stringify({
+            title: title,
+            description: description
+        }),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).then((resp) => {
+        resp.json().then(data => {
+
+            const parentElement = document.getElementById("mainArea");
+
+            const childElement = document.createElement("div");
+
+            const titleElement = document.createElement("span");
+            titleElement.innerHTML = data[i].title;
+
+            const descriptionElement = document.createElement("span");
+            descriptionElement.innerHTML = data[i].description;
+
+            const deleteButton = document.createElement("button");
+            deleteButton.innerHTML = "Delete";
+
+            childElement.appendChild(titleElement);
+            childElement.appendChild(descriptionElement);
+            childElement.appendChild(deleteButton);
+
+            parentElement.appendChild(childElement);
+
+        })
+    })
+}
