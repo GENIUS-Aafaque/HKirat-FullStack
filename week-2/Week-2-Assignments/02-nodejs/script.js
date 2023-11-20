10727181fe06167b04389b426405d05fca7667db
Md Aafaque Tabish <aafaque.tabish@gmail.com>
2023-11-21 00:57:31 +0530

    Add  : getTodos function to display all todos

---
+++ week-2/Week-2-Assignments/02-nodejs/script.js
@@ -0,0 +1,30 @@
+function getTodos() {
+    fetch("http://localhost:3000/todos", {
+        method: "GET"
+    }).then((resp) => {
+        resp.json().then(data => {
+            for (let i = 0; i < data.length; i++) {
+                const parentElement = document.getElementById("mainArea");
+
+                const childElement = document.createElement("div");
+
+                const titleElement = document.createElement("span");
+                titleElement.innerHTML = data[i].title;
+
+                const descriptionElement = document.createElement("span");
+                descriptionElement.innerHTML = data[i].description;
+
+                const deleteButton = document.createElement("button");
+                deleteButton.innerHTML = "Delete";
+
+                childElement.appendChild(titleElement);
+                childElement.appendChild(descriptionElement);
+                childElement.appendChild(deleteButton);
+
+                parentElement.appendChild(childElement);
+            }
+        })
+    })
+}
+getTodos();
+
