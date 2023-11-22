c7a00544726b198629a093cc066bfdec8495e71b
Md Aafaque Tabish <aafaque.tabish@gmail.com>
2023-11-22 15:00:37 +0530

    add : logic to list all courses

--- week-3/02-course-app-easy/index.js
+++ week-3/02-course-app-easy/index.js
@@ -99,6 +99,7 @@ app.post('/users/login', authenticateUser, (req, res) => {
 
 app.get('/users/courses', (req, res) => {
     // logic to list all courses
+    res.json({ courses: COURSES });
 });
 
 app.post('/users/courses/:courseId', (req, res) => {
