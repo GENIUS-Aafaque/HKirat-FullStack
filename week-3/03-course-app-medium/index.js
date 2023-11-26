b94b31f9f5a68bc2b1cb6be4346ace6ccf78cdce
Md Aafaque Tabish <aafaque.tabish@gmail.com>
2023-11-26 23:52:44 +0530

    add : user/admin authentication

--- week-3/03-course-app-medium/index.js
+++ week-3/03-course-app-medium/index.js
@@ -21,6 +21,24 @@ try {
 }
 console.log(ADMINS);
 
+const SECRET = 'S3cr31-keY';
+
+const authenticateJwt = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (authHeader) {
+        const token = authHeader.split(' ')[1];
+        jwt.verify(token, SECRET, (err, user) => {
+            if (err) {
+                return res.sendStatus(403);
+            }
+            req.user = user;
+            next();
+        });
+    } else {
+        res.sendStatus(401);
+    }
+};
+
 // Admin routes
 app.post('/admin/signup', (req, res) => {
     // logic to sign up admin
