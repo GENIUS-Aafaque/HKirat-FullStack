f43da0f407b3abe4a58caa906018ebc4f11f9b4c
Md Aafaque Tabish <aafaque.tabish@gmail.com>
2023-11-27 12:49:08 +0530

    add : authentication

--- week-3/04-course-app-hard/index.js
+++ week-3/04-course-app-hard/index.js
@@ -1,54 +1,69 @@
 const express = require('express');
 const app = express();
+const jwt = require('jsonwebtoken');
 
 app.use(express.json());
 
-let ADMINS = [];
-let USERS = [];
-let COURSES = [];
+const SECRET = "S3cr31-keY";
+
+const authenticateJwt = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (authHeader) {
+        const token = authHeader.split(' ')[1];
+        jwt.verify(token, SECRET, (err, user) => {
+            if (err) {
+                return res.sendStatus(403);
+            }
+            req.user = user;
+            next();
+        });
+    } else {
+        res.sendStatus(401);
+    }
+}
 
 // Admin routes
 app.post('/admin/signup', (req, res) => {
-  // logic to sign up admin
+    // logic to sign up admin
 });
 
 app.post('/admin/login', (req, res) => {
-  // logic to log in admin
+    // logic to log in admin
 });
 
 app.post('/admin/courses', (req, res) => {
-  // logic to create a course
+    // logic to create a course
 });
 
 app.put('/admin/courses/:courseId', (req, res) => {
-  // logic to edit a course
+    // logic to edit a course
 });
 
 app.get('/admin/courses', (req, res) => {
-  // logic to get all courses
+    // logic to get all courses
 });
 
 // User routes
 app.post('/users/signup', (req, res) => {
-  // logic to sign up user
+    // logic to sign up user
 });
 
 app.post('/users/login', (req, res) => {
-  // logic to log in user
+    // logic to log in user
 });
 
 app.get('/users/courses', (req, res) => {
-  // logic to list all courses
+    // logic to list all courses
 });
 
 app.post('/users/courses/:courseId', (req, res) => {
-  // logic to purchase a course
+    // logic to purchase a course
 });
 
 app.get('/users/purchasedCourses', (req, res) => {
-  // logic to view purchased courses
+    // logic to view purchased courses
 });
 
 app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
+    console.log('Server is listening on port 3000');
 });
