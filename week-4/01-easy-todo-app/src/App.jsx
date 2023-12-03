124015889397f517c7db2a6ef025dcb4d20269a6
Md Aafaque Tabish <aafaque.tabish@gmail.com>
2023-12-03 14:32:16 +0530

    add logic to render each todo

--- week-4/01-easy-todo-app/src/App.jsx
+++ week-4/01-easy-todo-app/src/App.jsx
@@ -18,7 +18,15 @@ function App() {
 
 function Todo(props) {
     // Add a delete button here so user can delete a TODO.
-    return <div>{props.title}</div>;
+    return (
+        <div id={props.id}>
+            {props.title}
+            {props.description}
+            <button onClick={document.removeChild(getElementById(props.id))}>
+                Delete
+            </button>
+        </div>
+    );
 }
 
 export default App;
