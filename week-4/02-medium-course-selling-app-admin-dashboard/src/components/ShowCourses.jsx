e0fd8ee5ce9f78955be0abb342aae0e31fc1775a
Md Aafaque Tabish <aafaque.tabish@gmail.com>
2023-12-05 23:49:27 +0530

    add logic to fetch existing courses

--- week-4/02-medium-course-selling-app-admin-dashboard/src/components/ShowCourses.jsx
+++ week-4/02-medium-course-selling-app-admin-dashboard/src/components/ShowCourses.jsx
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function ShowCourses() {
-    const [courses, setCourses] = React.useState([]);
+    const [courses, setCourses] = useState([]);
 
     // Add code to fetch courses from the server
     // and set it in the courses state variable.
+
+    useEffect(() => {
+        axios
+            .get("http://localhost:3000/admin/courses", {
+                headers: {
+                    username: "sameer",
+                    password: 123,
+                },
+            })
+            .then((response) => {
+                setCourses(response.data.courses);
+            })
+            .catch(() => console.log("error in fetching"));
+        setInterval(() => {
+            axios
+                .get("http://localhost:3000/admin/courses", {
+                    headers: {
+                        username: "sameer",
+                        password: 123,
+                    },
+                })
+                .then((response) => {
+                    setCourses(response.data.courses);
+                })
+                .catch(() => console.log("error in fetching"));
+        }, 1000);
+    }, []);
+
     return (
         <div>
             <h1>Show Courses Page</h1>
