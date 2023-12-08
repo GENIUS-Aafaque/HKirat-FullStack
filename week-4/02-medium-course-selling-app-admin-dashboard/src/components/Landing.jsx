8d3b14d0a26af0a674d595cf82779b304a5836d0
Md Aafaque Tabish <aafaque.tabish@gmail.com>
2023-12-08 21:19:59 +0530

    create landing page(add basic styling)

--- week-4/02-medium-course-selling-app-admin-dashboard/src/components/Landing.jsx
+++ week-4/02-medium-course-selling-app-admin-dashboard/src/components/Landing.jsx
@@ -1,15 +1,52 @@
 import React from "react";
+import Card from "@mui/material/Card";
+import Button from "@mui/material/Button";
+import { Typography } from "@mui/material";
 
 /// This is the landing page. You need to add a link to the login page here.
 /// Maybe also check from the backend if the user is already logged in and then show them a logout button
 /// Logging a user out is as simple as deleting the token from the local storage.
 function Landing() {
     return (
-        <div>
-            <h1>Welcome to course selling website!</h1>
-            <a href="/register">Register</a>
-            <br />
-            <a href="/login">Login</a>
+        <div
+            style={{
+                display: "grid",
+                justifyContent: "center",
+                paddingTop: 200,
+            }}
+        >
+            <Card
+                variant="outlined"
+                style={{
+                    width: 400,
+                    padding: 40,
+                    outlineWidth: 4,
+                }}
+            >
+                <Typography
+                    variant="h4"
+                    style={{ flex: "1", color: "#aaaaaa" }}
+                >
+                    <u>Welcome to COURZERO!</u>
+                </Typography>
+                <br />
+                <br />
+
+                <div
+                    style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        gap: 64,
+                    }}
+                >
+                    <Button variant="contained" size="large">
+                        Register
+                    </Button>
+                    <Button variant="contained" size="large">
+                        Login
+                    </Button>
+                </div>
+            </Card>
         </div>
     );
 }
