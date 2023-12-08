8b9618cd8a0df71dc207e74d5dacf328159be98e
Md Aafaque Tabish <aafaque.tabish@gmail.com>
2023-12-08 21:23:56 +0530

    add: functionality to buttons

--- week-4/02-medium-course-selling-app-admin-dashboard/src/components/Landing.jsx
+++ week-4/02-medium-course-selling-app-admin-dashboard/src/components/Landing.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import Card from "@mui/material/Card";
 import Button from "@mui/material/Button";
 import { Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 /// This is the landing page. You need to add a link to the login page here.
 /// Maybe also check from the backend if the user is already logged in and then show them a logout button
 /// Logging a user out is as simple as deleting the token from the local storage.
 function Landing() {
+    const navigate = useNavigate();
+
     return (
         <div
             style={{
@@ -39,10 +42,22 @@ function Landing() {
                         gap: 64,
                     }}
                 >
-                    <Button variant="contained" size="large">
+                    <Button
+                        variant="contained"
+                        size="large"
+                        onClick={() => {
+                            navigate("/register");
+                        }}
+                    >
                         Register
                     </Button>
-                    <Button variant="contained" size="large">
+                    <Button
+                        variant="contained"
+                        size="large"
+                        onClick={() => {
+                            navigate("/login");
+                        }}
+                    >
                         Login
                     </Button>
                 </div>
