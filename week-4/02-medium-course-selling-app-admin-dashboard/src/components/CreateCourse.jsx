ffe6d0fb295d6018d28fc07eacf1689b05ec6048
Md Aafaque Tabish <aafaque.tabish@gmail.com>
2023-12-08 23:33:29 +0530

    create : component for creating a new course

--- week-4/02-medium-course-selling-app-admin-dashboard/src/components/CreateCourse.jsx
+++ week-4/02-medium-course-selling-app-admin-dashboard/src/components/CreateCourse.jsx
@@ -2,13 +2,39 @@ import React from "react";
 /// You need to add input boxes to take input for users to create a course.
 /// I've added one input so you understand the api to do it.
 function CreateCourse() {
-    const [title, setTitle] = React.useState("");
+    const [course, setCourse] = React.useState({
+        title: "",
+        description: "",
+        price: 0,
+    });
+
+    const handleInputChange = (e) => {
+        setCourse((oldValue) => {
+            return { ...oldValue, [e.target.name]: e.target.value };
+        });
+    };
 
     return (
         <div>
             <h1>Create Course Page</h1>
-            <input type={"text"} onChange={(e) => setTitle(e.target.value)} />
-            <button onClick={() => console.log(title)}>Create Course</button>
+            <input
+                type={"text"}
+                name="title"
+                value={course.title}
+                onChange={handleInputChange}
+            />
+            <textarea
+                name="description"
+                value={course.description}
+                onChange={handleInputChange}
+            />
+            <input
+                type={"number"}
+                name="price"
+                value={course.price}
+                onChange={handleInputChange}
+            />
+            <button onClick={() => console.log(course)}>Create Course</button>
         </div>
     );
 }
