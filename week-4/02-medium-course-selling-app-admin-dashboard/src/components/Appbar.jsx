63d168c8032db0e87f6e14f370526b6c69fdff4a
Md Aafaque Tabish <aafaque.tabish@gmail.com>
2023-12-08 22:52:08 +0530

    add : logic and styling for logout

--- week-4/02-medium-course-selling-app-admin-dashboard/src/components/Appbar.jsx
+++ week-4/02-medium-course-selling-app-admin-dashboard/src/components/Appbar.jsx
@@ -1,10 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "@mui/material";
 import { Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function Appbar() {
     const navigate = useNavigate();
+    const [username, setUsername] = useState(null);
+
+    useEffect(() => {
+        axios
+            .get("http://localhost:3000/admin/me", {
+                headers: {
+                    authorization: "Bearer " + localStorage.getItem("token"),
+                },
+            })
+            .then((response) => {
+                setUsername(response.data.username);
+            });
+    }, []);
+
+    if (username) {
+        return (
+            <div
+                style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    padding: 12,
+                    backgroundColor: "#dddddd",
+                    borderBottom: "2px solid #bbbbbb",
+                }}
+            >
+                <div>
+                    <Typography variant="h4">COURZERO</Typography>
+                </div>
+                <div style={{ display: "flex", gap: 8, alignItems: "center" }}>
+                    <Typography variant="h6">{username}</Typography>
+                    <Button
+                        variant="contained"
+                        onClick={() => {
+                            navigate("/");
+                            localStorage.setItem("token", null);
+                        }}
+                    >
+                        Log Out
+                    </Button>
+                </div>
+            </div>
+        );
+    }
+
     return (
         <div
             style={{
