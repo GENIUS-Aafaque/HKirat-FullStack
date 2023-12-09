2cc8bb326a480e6186f22027148428bceec86b0f
Md Aafaque Tabish <aafaque.tabish@gmail.com>
2023-12-09 23:18:49 +0530

    Modify: Create course page - styling

--- week-4/02-medium-course-selling-app-admin-dashboard/src/components/CreateCourse.jsx
+++ week-4/02-medium-course-selling-app-admin-dashboard/src/components/CreateCourse.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Card from "@mui/material/Card";
+import Button from "@mui/material/Button";
+import { TextField } from "@mui/material";
+import { Typography } from "@mui/material";
 /// You need to add input boxes to take input for users to create a course.
 /// I've added one input so you understand the api to do it.
 function CreateCourse() {
     const [course, setCourse] = React.useState({
         title: "",
         description: "",
-        price: 0,
+        price: "",
     });
 
     const handleInputChange = (e) => {
@@ -15,26 +19,61 @@ function CreateCourse() {
     };
 
     return (
-        <div>
-            <h1>Create Course Page</h1>
-            <input
-                type={"text"}
-                name="title"
-                value={course.title}
-                onChange={handleInputChange}
-            />
-            <textarea
-                name="description"
-                value={course.description}
-                onChange={handleInputChange}
-            />
-            <input
-                type={"number"}
-                name="price"
-                value={course.price}
-                onChange={handleInputChange}
-            />
-            <button onClick={() => console.log(course)}>Create Course</button>
+        <div
+            style={{
+                width: "100%",
+                height: "100vh",
+                backgroundColor: "#eeeeee",
+                display: "grid",
+                placeItems: "center",
+            }}
+        >
+            <div style={{ marginTop: -100 }}>
+                <Typography align={"center"} variant="h4">
+                    Create New Course
+                </Typography>
+                <Card variant="outlined" style={{ padding: 20, width: 300 }}>
+                    <TextField
+                        fullWidth
+                        label="Title"
+                        variant="outlined"
+                        type={"text"}
+                        name="title"
+                        value={course.title}
+                        onChange={handleInputChange}
+                    />
+                    <br />
+                    <br />
+                    <TextField
+                        fullWidth
+                        label="Description"
+                        variant="outlined"
+                        type={"text"}
+                        name="description"
+                        value={course.description}
+                        onChange={handleInputChange}
+                    />
+                    <br />
+                    <br />
+                    <TextField
+                        fullWidth
+                        label="Price"
+                        variant="outlined"
+                        type={"number"}
+                        name="price"
+                        value={course.price}
+                        onChange={handleInputChange}
+                    />
+                    <br />
+                    <br />
+                    <Button
+                        variant="contained"
+                        onClick={() => console.log(course)}
+                    >
+                        Create Course
+                    </Button>
+                </Card>
+            </div>
         </div>
     );
 }
